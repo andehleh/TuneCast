30ff3f6974af2a69b0fa280a7a6724f6eaaaffd6
Eman <emanamberzindani@gmail.com>
2023-03-27 12:29:55 -0500

    setup mongoDB

---
+++ sample_service/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class ThingParams(BaseModel):
+    name: str
+
+
+class Thing(ThingParams):
+    id: str
+
+
+class ThingsList(BaseModel):
+    things: list[Thing]
---
+++ sample_service/queries/client.py
@@ -0,0 +1,12 @@
+import os
+import pymongo
+
+MONGO_URL = os.environ.get('MONGO_URL', 'TEST')
+client = pymongo.MongoClient(MONGO_URL)
+
+
+class Queries:
+    @property
+    def collection(self):
+        db = client['things']
+        return db[self.COLLECTION]

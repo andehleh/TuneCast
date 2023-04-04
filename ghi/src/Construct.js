46d2b321a2e3fb523ae3db380ea2c4d4a93bd685
Taylor <taylor@roadrunnercharters.com>
2023-04-04 13:00:58 -0500

    Getting react/node figured out for everyone

--- ghi/src/Construct.js
+++ ghi/src/Construct.js
@@ -9,7 +9,7 @@ function Construct(props) {
                 <h2>Coming on (or before)</h2>
                 <h2>{props.info.year}-{pad2(props.info.month)}-{pad2(props.info.day)}</h2>
                 <h2>by or <strong>WELL BEFORE</strong> {pad2(props.info.hour)}:{pad2(props.info.min)}</h2>
-                
+
             </header>
         </div>
     )

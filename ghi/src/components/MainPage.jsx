86d8bf0016f0e24c589d085248b4373de933cb63
Andy <choongho98@gmail.com>
2023-04-07 15:17:28 -0400

    got spotify to display and play on mainpage

--- ghi/src/components/MainPage.jsx
+++ ghi/src/components/MainPage.jsx
@@ -1,17 +1,53 @@
-
+import { useState, useEffect } from "react";
 
 const MainPage = () => {
 
+  const [weather, setWeather] = useState([]);
+
+  async function getData() {
+    const resp = await fetch("http://localhost:8000/api/weather/");
+    if (resp.ok) {
+      const data = await resp.json();
+      console.log(data.weather)
+      setWeather(data.weather);
+      console.log(weather)
+    }
+  }
+
+  useEffect(() => {
+    getData();
+    console.log(weather)
+  }, []);
+
+
   return (
     <>
-    <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">Tunecast</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
-          Weather-Based Playlist Generator!
-        </p>
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">Tunecast</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">Weather-Based Playlist Generator!</p>
+          <div>
+            <select>
+              <option>Sunny</option>
+              <option>Rainy</option>
+              <option>Cloudy</option>
+              <option>Snowy</option>
+            </select>
+          </div>
+          <div>
+            <iframe
+              style={{ borderRadius: "12px" }}
+              src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ?utm_source=generator&theme=0"
+              width="100%"
+              height="352"
+              frameBorder="0"
+              allowFullScreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };

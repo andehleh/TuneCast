08dcff75881fd84810d199ef58c33ac2dc11d5bb
Johnathan <jonathanhas9@gmail.com>
2023-04-17 17:38:22 -0400

    Create Playlist Unit test

--- TuneCast/tests/test_playlist.py
+++ TuneCast/tests/test_playlist.py
@@ -1,11 +1,22 @@
 from fastapi.testclient import TestClient
 from main import app
 from queries.playlist import PlaylistRepo
+from models import Playlist, PlaylistOut
 
 client = TestClient(app)
 
 class FakePlaylistRepo:
     def get_all(self):
+        return [
+          {
+            "url": "https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ",
+            "name": "Deep Focus",
+            "weather": "Rainy",
+            "id": "643d764424fea7afc46a5ae6"
+          }
+        ]
+
+    def create(self):
         return [
           {
             "url": "https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ",
@@ -21,6 +32,22 @@ def test_list_playlist():
 
     #Act
     res = client.get('/api/playlist/')
+    data = res.json()
+
+    #Assert
+    assert res.status_code == 200
+    assert type(data['playlist']) == list
+
+    #Cleanup
+    app.dependency_overrides = {}
+
+
+def test_create_playlist():
+    #Arrange
+    app.dependency_overrides[PlaylistRepo] = FakePlaylistRepo
+
+    #Act
+    res = client.post('/api/playlist')
     data = res.json()
 
     #Assert

71098f4b835a54b9af7e7240229896e0ac122ce1
Johnathan <jonathanhas9@gmail.com>
2023-04-28 18:20:32 -0400

    Added scrollbar to history modal fixed invisible words and  buttons on login/signup page

--- TuneCast/tests/test_weather_api.py
+++ TuneCast/tests/test_weather_api.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.weather_api import OpenWeatherRepo
+from queries.weather_api import OpenWeatherRepo, OpenWeatherGeoRepo
 
 client = TestClient(app)
 
@@ -18,6 +18,52 @@ class FakeOpenWeatherRepo:
             ]
         }
 
+class FakeOpenWeatherGeoRepo:
+    def get_current_weather(self, lon: float, lat: float):
+        return {
+            "coord": {
+                "lon": -97.1055,
+                "lat": 32.6077
+            },
+            "weather": [
+                {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04d"
+                }
+            ],
+            "base": "stations",
+            "main": {
+                "temp": 65.48,
+                "feels_like": 65.97,
+                "temp_min": 62.47,
+                "temp_max": 67.89,
+                "pressure": 1008,
+                "humidity": 90
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 9.22,
+                "deg": 320
+            },
+            "clouds": {
+                "all": 100
+            },
+            "dt": 1682719499,
+            "sys": {
+                "type": 2,
+                "id": 2021119,
+                "country": "US",
+                "sunrise": 1682682275,
+                "sunset": 1682730421
+            },
+            "timezone": -18000,
+            "id": 4709013,
+            "name": "Mansfield",
+            "cod": 200
+            }
+
 
 def test_get_current_weather():
     #Arrange
@@ -26,6 +72,23 @@ def test_get_current_weather():
     state="tx"
     #Act
     res = client.get(f'/api/open_weather_api/{city}/{state}/')
+    data = res.json()
+
+    #Assert
+    assert res.status_code == 200
+    assert type(data) == dict
+
+    #Cleanup
+    app.dependency_overrides = {}
+
+def test_get_current_weather_lonlat():
+    #Arrange
+    app.dependency_overrides[OpenWeatherGeoRepo] = FakeOpenWeatherGeoRepo
+    lon = -97.1082727
+    lat = 32.6137264
+
+    #Act
+    res = client.get(f'/api/open_weather_api/{lon}_{lat}/')
     data = res.json()
 
     #Assert
